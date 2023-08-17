122080380bcdf20232c7100539156eca20c5956e
Dominique <domi.github@gmail.com>
2023-08-17 13:41:28 +0200

    fixup! style(whitespace): fix whitespace issues

--- android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/TimeSet.java
+++ android-nrf-mesh-library/meshprovisioner/src/main/java/no/nordicsemi/android/meshprovisioner/transport/TimeSet.java
@@ -69,6 +69,5 @@ public class TimeSet extends GenericMessage {
         bitWriter.write(taiTime.getTaiSeconds(), TAI_SECONDS_BIT_SIZE);
 
         mParameters = ArrayUtils.reverseArray(bitWriter.toByteArray());
-
     }
 }
